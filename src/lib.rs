d1ceec05e87c897be93d32f67a65f2771ee62da6
Brian Steffens <briansteffens@gmail.com>
2016-05-13 23:07:34 -0400

    Add empty case

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ pub fn parse(querystring: &str) -> HashMap<String, String> {
     let mut ret = HashMap::new();
 
     for part in querystring.split("&") {
+        if part == "" {
+            continue;
+        }
+
         let kv: Vec<&str> = part.split('=').collect();
 
         if kv.len() != 2 {
@@ -21,7 +25,7 @@ mod tests {
     use parse;
 
     #[test]
-    fn it_works() {
+    fn typical_case() {
         let parsed = parse("hi=hello&hey=7");
 
         assert_eq!(2, parsed.len());
@@ -31,5 +35,12 @@ mod tests {
 
         assert!(parsed.contains_key("hey"));
         assert_eq!(parsed.get("hey").unwrap(), "7");
+    }
+
+    #[test]
+    fn empty() {
+        let parsed = parse("");
+
+        assert_eq!(0, parsed.len());
     }
 }
