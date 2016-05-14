17d1ac1e079e0f1d1b058820f6fe2b8dc0cdb8db
Brian Steffens <briansteffens@gmail.com>
2016-05-13 22:58:52 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+pub fn parse(querystring: &str) -> HashMap<String, String> {
+    let mut ret = HashMap::new();
+
+    for part in querystring.split("&") {
+        let kv: Vec<&str> = part.split('=').collect();
+
+        if kv.len() != 2 {
+            panic!("Invalid querystring format");
+        }
+
+        ret.insert(String::from(kv[0]), String::from(kv[1]));
+    }
+
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use parse;
+
+    #[test]
+    fn it_works() {
+        let parsed = parse("hi=hello&hey=7");
+
+        assert_eq!(2, parsed.len());
+
+        assert!(parsed.contains_key("hi"));
+        assert_eq!(parsed.get("hi").unwrap(), "hello");
+
+        assert!(parsed.contains_key("hey"));
+        assert_eq!(parsed.get("hey").unwrap(), "7");
+    }
+}
