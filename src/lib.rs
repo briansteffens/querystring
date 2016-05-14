28f7f01e542dbdc7d86e5b504393c3c1ae58e90c
Brian Steffens <briansteffens@gmail.com>
2016-05-14 06:59:28 -0400

    Add error handling and more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
-pub fn parse(querystring: &str) -> HashMap<String, String> {
+pub fn parse(querystring: &str)
+             -> Result<HashMap<String, String>, &'static str> {
     let mut ret = HashMap::new();
 
     for part in querystring.split("&") {
@@ -11,13 +12,13 @@ pub fn parse(querystring: &str) -> HashMap<String, String> {
         let kv: Vec<&str> = part.split('=').collect();
 
         if kv.len() != 2 {
-            panic!("Invalid querystring format");
+            return Err("Invalid querystring format");
         }
 
         ret.insert(String::from(kv[0]), String::from(kv[1]));
     }
 
-    ret
+    Ok(ret)
 }
 
 #[cfg(test)]
@@ -26,9 +27,9 @@ mod tests {
 
     #[test]
     fn typical_case() {
-        let parsed = parse("hi=hello&hey=7");
+        let parsed = parse("hi=hello&hey=7").unwrap();
 
-        assert_eq!(2, parsed.len());
+        assert_eq!(parsed.len(), 2);
 
         assert!(parsed.contains_key("hi"));
         assert_eq!(parsed.get("hi").unwrap(), "hello");
@@ -38,9 +39,34 @@ mod tests {
     }
 
     #[test]
-    fn empty() {
-        let parsed = parse("");
+    fn single_pair() {
+        let parsed = parse("greetings=hello").unwrap();
 
-        assert_eq!(0, parsed.len());
+        assert_eq!(parsed.len(), 1);
+
+        assert!(parsed.contains_key("greetings"));
+        assert_eq!(parsed.get("greetings").unwrap(), "hello");
+    }
+
+    #[test]
+    fn no_pairs() {
+        let parsed = parse("").unwrap();
+
+        assert_eq!(parsed.len(), 0);
+    }
+
+    #[test]
+    fn empty_value() {
+        let parsed = parse("nothing=").unwrap();
+
+        assert_eq!(parsed.len(), 1);
+
+        assert!(parsed.contains_key("nothing"));
+        assert_eq!(parsed.get("nothing").unwrap(), "");
+    }
+
+    #[test]
+    fn extra_equal_signs_in_kvp() {
+        assert!(parse("this=is=wrong").is_err());
     }
 }
